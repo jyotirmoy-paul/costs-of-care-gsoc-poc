2ccc68ff22d1bc28bdb19412ad44e17b7e6651ba
Jyotirmoy Paul <cipherresfeber@gmail.com>
2020-03-21 07:50:36 +0530

    added screenshots

--- api/sample/models.py
+++ api/sample/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Hospitals(models.Model):
     hospital_name = models.CharField(max_length=200)
     procedure_name = models.CharField(max_length=200)
-    estimated_cost = models.DecimalField(decimal_places=2, max_digits=1000000)
+    estimated_cost = models.DecimalField(decimal_places=2,
+        max_digits=1000000)
